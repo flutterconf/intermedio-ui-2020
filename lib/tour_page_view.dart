34edd6289ba58568d2d576a60d712098612b2f43
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-12-03 18:40:02 +0000

    Included swiper

--- lib/tour_page_view.dart
+++ lib/tour_page_view.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 import 'data/tour.dart';
 
 class TourCarrousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Suerte con el challenge :)
     return Center(
-      child: Text('3...2...1... GOOO!!'),
+      child: Swiper(
+        itemCount: Tour.defaultList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _TourContainer(Tour.defaultList[index]);
+        },
+        viewportFraction: 0.7,
+        scale: 0.85,
+      ),
     );
-    
+  }
+}
+
+class _TourContainer extends StatelessWidget {
+  _TourContainer(this.tour);
+
+  final Tour tour;
+  // AssetImage(Tour.defaultList[index].imageUrl)
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(tour.imageUrl), fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(20)),
+    );
   }
 }
