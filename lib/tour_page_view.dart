eefaa73eb697d62bba508b29d61689a1d82e42f7
unknown <Personal@WPU8L0083796.ad.ing.net>
2020-12-03 20:29:52 +0100

    Add animatino

--- lib/tour_page_view.dart
+++ lib/tour_page_view.dart
@@ -17,33 +17,36 @@ class TourCarrousel extends StatelessWidget {
         return Builder(
           builder: (BuildContext context) {
             return InkWell(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage(
-                      i.imageUrl,
+              child: Hero(
+                tag: i.id,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        i.imageUrl,
+                      ),
+                      fit: BoxFit.cover,
                     ),
-                    fit: BoxFit.cover,
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        i.title,
-                        style: titleStyle,
-                      ),
-                      Text(
-                        i.subtitle,
-                        style: subtitleStyle,
-                      ),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          i.title,
+                          style: titleStyle,
+                        ),
+                        Text(
+                          i.subtitle,
+                          style: subtitleStyle,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -64,23 +67,10 @@ class TourCarrousel extends StatelessWidget {
   }
 }
 
-class FullScreenTour extends StatefulWidget {
+class FullScreenTour extends StatelessWidget {
   final Tour selectedItem;
 
   const FullScreenTour({Key key, this.selectedItem}) : super(key: key);
-
-  @override
-  _FullScreenTourState createState() => _FullScreenTourState();
-}
-
-class _FullScreenTourState extends State<FullScreenTour> {
-  final CarouselController _controller = CarouselController();
-
-  @override
-  void initState() {
-    _controller.jumpToPage(Tour.defaultList.indexOf(widget.selectedItem));
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,44 +78,50 @@ class _FullScreenTourState extends State<FullScreenTour> {
     final theme = Theme.of(context);
     final titleStyle = theme.textTheme.headline6.copyWith(color: Colors.white);
     final subtitleStyle = theme.textTheme.caption.copyWith(color: Colors.white);
+
+    final initialPage = Tour.defaultList.indexOf(selectedItem);
+
     return Scaffold(
       body: CarouselSlider(
-        carouselController: _controller,
         options: CarouselOptions(
           height: size.height,
           viewportFraction: 1.0,
           enlargeCenterPage: false,
+          initialPage: initialPage,
         ),
         items: Tour.defaultList.map((i) {
           return Builder(
             builder: (BuildContext context) {
               return Stack(
                 children: [
-                  Container(
-                    width: size.width,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          i.imageUrl,
+                  Hero(
+                    tag: i.id,
+                    child: Container(
+                      width: size.width,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            i.imageUrl,
+                          ),
+                          fit: BoxFit.cover,
                         ),
-                        fit: BoxFit.cover,
                       ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            i.title,
-                            style: titleStyle,
-                          ),
-                          Text(
-                            i.subtitle,
-                            style: subtitleStyle,
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              i.title,
+                              style: titleStyle,
+                            ),
+                            Text(
+                              i.subtitle,
+                              style: subtitleStyle,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
